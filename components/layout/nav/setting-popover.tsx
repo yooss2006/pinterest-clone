95412c22c73eebf308a6f56c674a149b5c7686ec
yooss2006 <yooss2006@naver.com>
2025-03-24 23:42:28 +0900

    Feat: setting 팝오버 메뉴 hover시 활성화 상태 관리

--- components/layout/nav/setting-popover.tsx
+++ components/layout/nav/setting-popover.tsx
@@ -2,23 +2,52 @@
 
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { SquareArrowOutUpRight } from "lucide-react";
 
+const buttonOption = {
+  setting: { options: ["설정", "홈피드 조정"], withIcon: false },
+  governance: {
+    options: [
+      "외부 계정 소유권 표시",
+      "Chrome 앱 설치",
+      "신고 및 위반 센터",
+      "개인정보 보호권",
+    ],
+    withIcon: false,
+  },
+  resources: {
+    options: [
+      "도움말 센터",
+      "서비스 약관",
+      "개인정보처리방침",
+      "베타 테스터 되기",
+    ],
+    withIcon: true,
+  },
+};
+
 const commonStyle =
-  "w-full h-fit px-4 py-3 justify-between font-semibold text-base/5 focus:bg-gray-100 focus-visible:shadow-none";
+  "w-full h-fit px-4 py-3 justify-between font-semibold text-base/5 focus-visible:shadow-none";
 
 const SettingList = ({
   items,
   withIcon = false,
+  active,
 }: {
   items: string[];
   withIcon?: boolean;
+  active: string;
 }) => (
   <ul className="mb-4 last:mb-0">
     {items.map((text) => (
       <li key={`setting-${text}`}>
-        <Button className={cn(commonStyle)} variant="ghost" title={text}>
+        <Button
+          className={cn(commonStyle, active === text && "bg-gray-100")}
+          variant="ghost"
+          title={text}
+          data-setting={text}
+        >
           <span>{text}</span>
           {withIcon && <SquareArrowOutUpRight />}
         </Button>
@@ -28,36 +57,45 @@ const SettingList = ({
 );
 
 export default function SettingPopover() {
-  const sectionRef = useRef<HTMLElement>(null);
+  const [active, setActive] = useState(buttonOption.setting.options[0]);
+  const sectionRef = useRef<HTMLElement | null>(null);
 
   useEffect(() => {
-    const button = sectionRef.current?.querySelector("ul button");
-    if (button) {
-      (button as HTMLButtonElement).focus();
+    const section = sectionRef.current;
+
+    const handleHover = (e: MouseEvent) => {
+      const target = e.target as HTMLElement;
+
+      const button = target.closest(
+        "button[data-setting]"
+      ) as HTMLElement | null;
+
+      if (button) {
+        const text = button.getAttribute("data-setting");
+        if (text) setActive(text);
+      }
+    };
+
+    if (section) {
+      section.addEventListener("mouseover", handleHover, true);
     }
+
+    return () => {
+      section?.removeEventListener("mouseover", handleHover, true);
+    };
   }, []);
 
   return (
     <section className="p-2" ref={sectionRef}>
       <h2 className="sr-only">설정</h2>
-      <SettingList items={["설정", "홈피드 조정"]} />
-      <SettingList
-        items={[
-          "외부 계정 소유권 표시",
-          "Chrome 앱 설치",
-          "신고 및 위반 센터",
-          "개인정보 보호권",
-        ]}
-      />
-      <SettingList
-        items={[
-          "도움말 센터",
-          "서비스 약관",
-          "개인정보처리방침",
-          "베타 테스터 되기",
-        ]}
-        withIcon
-      />
+      {Object.values(buttonOption).map(({ options, withIcon }, index) => (
+        <SettingList
+          key={`setting-list-${index}`}
+          items={options}
+          withIcon={withIcon}
+          active={active}
+        />
+      ))}
     </section>
   );
 }
